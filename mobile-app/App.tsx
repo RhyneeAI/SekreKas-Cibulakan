b5458dfb7843dd86b592d909c9b7df373063e02e
Luhung Lugina <luhung.tea@gmail.com>
2026-07-10 07:43:07 +0700

    feat(mobile): tambah NativeWind (Tailwind untuk React Native)

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,3 +1,4 @@
+import "./global.css";
 import { ActivityIndicator, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
